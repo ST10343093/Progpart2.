9e37afbbd6d1845449bc536a67e9e02e84c2acb5
ST10343093 <128588000+ST10343093@users.noreply.github.com>
2024-05-30 19:33:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,52 @@
-﻿    namespace ST10343093
+
+/*  Code attribution
+ *  The follwing code was taken from StackOverFlow:
+ *  Link : https://stackoverflow.com/questions/2552501/switch-statement-in-c-sharp
+ *  switch (x)
+{
+   case 1:
+     //do something
+     break;
+   case 2..8:
+     //do something else
+     break;
+   default:
+     break;
+}
+
+
+    The follwing code was taken from StackOverFlow:
+ *  Link : https://stackoverflow.com/questions/14973642/how-using-try-catch-for-exception-handling-is-best-practice
+ *  try
+{
+    //do something
+}
+    catch
+{
+    //Do nothing
+} 
+ *  
+ *  The follwing code was taken from StackOverFlow:
+ *  Link : The follwing code was taken from StackOverFlow: 
+ *  https://stackoverflow.com/questions/14973642/how-using-try-catch-for-exception-handling-is-best-practice
+ * 
+ *   for (i=0 ; i<=10; i++)
+{
+    ..
+    ..
+}
+
+    i=0;
+    while(i<=10)
+    {
+        ..
+        ..
+        i++;
+    }
+    END
+ */ 
+
+namespace ST10343093
     {
         internal class Program
         {
